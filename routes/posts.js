4a09a52b7f51758fce8ff144b80aefa556128856
Aniket Dutta <aniket2018dutta@gmail.com>
2021-11-24 05:07:03 +0530

    add a controller and route for users and posts

---
+++ routes/posts.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const postsController = require('../controllers/posts_controller');
+router.get('/like',postsController.like);
+
+module.exports = router;
