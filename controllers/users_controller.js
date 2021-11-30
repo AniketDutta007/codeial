1273a1d45c541c833da28edac42a6c3a170761a7
Aniket Dutta <aniket2018dutta@gmail.com>
2021-11-30 07:41:46 +0530

    setup initial schema for user and views for sign up and sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,27 @@
+// render the profile page
 module.exports.profile = function(req, res){
     return res.render('profile',{
         title: "Aniket Dutta",
         userName: "Aniket Dutta"
     });
+};
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: 'Codeial : SignUp'
+    });
+};
+// render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: 'Codeial : SignIn'
+    });
+};
+// get the sign up data
+module.exports.create = function(req, res){
+
+};
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+
 };
